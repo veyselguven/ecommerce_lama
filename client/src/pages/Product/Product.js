15a668be255559f296c7579656f2f7837aa0f953
veysel basbaydar <vgbasbaydar@gmail.com>
2022-12-18 21:03:22 -0800

    slider component is done

--- client/src/pages/Product/Product.js
+++ client/src/pages/Product/Product.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Product.scss";
 
 function Product() {
-  return <div>Product</div>;
+  return <div className="product">Product</div>;
 }
 
 export default Product;
