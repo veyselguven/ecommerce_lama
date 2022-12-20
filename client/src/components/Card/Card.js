28b2010f087d69c494545c6a19027078cd86876d
veysel basbaydar <vgbasbaydar@gmail.com>
2022-12-19 16:26:49 -0800

    card component is almost done

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -4,8 +4,19 @@ import { Link } from "react-router-dom";
 function Card({ item }) {
   console.log("items=>", item);
   return (
-    <Link to={`/product/${item.id}`}>
-      <div className="card">Card</div>
+    <Link className="link" to={`/product/${item.id}`}>
+      <div className="card">
+        <div className="image">
+          {item.isNew && <span>New Season</span>}
+          <img src={item.img} alt="" className="mainImg" />
+          <img src={item.img2} alt="" className="secondImg" />
+        </div>
+        <h2>{item.title}</h2>
+        <div className="prices">
+          <h3>${item.oldPrice}</h3>
+          <h3>${item.price}</h3>
+        </div>
+      </div>
     </Link>
   );
 }
