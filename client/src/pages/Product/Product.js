23cd0ef2a62707064d0745b619fb3e1448aee86c
veysel basbaydar <vgbasbaydar@gmail.com>
2022-12-19 22:48:36 -0800

    product component is done

--- client/src/pages/Product/Product.js
+++ client/src/pages/Product/Product.js
@@ -1,8 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Product.scss";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
 
 function Product() {
-  return <div className="product">Product</div>;
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+
+  const images = [
+    "https://assets.entrepreneur.com/images/misc/1664403208_ent22_octnov_100Women_LauraBehrensWu-list.jpg",
+    "https://assets.entrepreneur.com/images/misc/1664382128_ent22_octnov_100Women_DavidaHerzl.jpg",
+  ];
+  return (
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <h1>Title</h1>
+        <span className="price">$199</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi ratione
+          eligendi perferendis, tempore eius dolorem perspiciatis, sed pariatur
+          repellat nam aliquam nulla tempora temporibus facere. Beatae, nobis.
+          Dolor, maiores ducimus.
+        </p>
+        <div className="quantity">
+          <button
+            onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+        </div>
+        <button className="add">
+          <AddShoppingCartIcon /> Add to Cart
+        </button>
+        <div className="links">
+          <div className="item">
+            <FavoriteBorderIcon /> Add to wish List
+          </div>
+          <div className="item">
+            <BalanceIcon /> Add to compare
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendor: Polo</span>
+          <span>Product Type: T-Shirt</span>
+          <span>Tag: T-Shirt, Women, Top</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFORMATION</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
